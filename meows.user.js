a1552b0311c99733fa44a302abe98314d0ef9a07
Meows <160031814+aodhub2329@users.noreply.github.com>
2025-08-26 13:16:27 +0700

    Create meows.user.js

---
+++ meows.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Meow Tiện Ích
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Meow tiện ích dành cho những người cụt tay
+// @author       You
+// @match        https://*.vsphone.com
+// @match        https://www.ugphone.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js
+// @require      https://raw.githubusercontent.com/aodhub2329/repo/main/extension.js
+// ==/UserScript==
